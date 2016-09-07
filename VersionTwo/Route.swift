6008ace4375bf5802e49043edad7c55bdbad2598
Lauren N. Roth <laurenroth@gmail.com>
2016-09-07 10:38:30 -0400

    Add tableview, filters button and navigation ui

---
+++ VersionTwo/Route.swift
@@ -0,0 +1,19 @@
+//
+//  Route.swift
+//  VersionTwo
+//
+//  Created by Lauren Nicole Roth on 9/7/16.
+//  Copyright © 2016 Skedaddle. All rights reserved.
+//
+
+import Foundation
+
+class Route {
+  var title: String
+  var photoURL: String
+  
+  init(title: String, photoURL: String) {
+    self.title = title
+    self.photoURL = photoURL
+  }
+}
