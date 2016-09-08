d42d91b5b69952f522f323af790dbf598c9dc1af
Lauren N. Roth <laurenroth@gmail.com>
2016-09-08 11:14:56 -0400

    Finish adding stubbed routes for now.

--- VersionTwo/RoutesAPIManager.swift
+++ VersionTwo/RoutesAPIManager.swift
@@ -19,8 +19,14 @@ class RouteAPIManager {
       let boston = Route(title: "Going to Boston", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/2e38ac30-e324-4bb2-95da-73d64f7f6f16/image-24.jpg")
       let toronto = Route(title: "Going to Toronto", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/08aa2c57-9703-4db7-8af9-9aef9e945b8d/image-18.jpg")
       let cape_cod = Route(title: "Going to Cape Cod", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/5138b799-9363-4aa7-b909-49e4d5bac5ad/image-26.jpg")
+      let tetons = Route(title: "Going to Tetons", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/2e38ac30-e324-4bb2-95da-73d64f7f6f16/image-24.jpg")
+      let boulder = Route(title: "Going to Boulder", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/08aa2c57-9703-4db7-8af9-9aef9e945b8d/image-18.jpg")
+      let los_angeles = Route(title: "Going to Los Angeles", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/5138b799-9363-4aa7-b909-49e4d5bac5ad/image-26.jpg")
+      let chicago = Route(title: "Going to Chicago", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/2e38ac30-e324-4bb2-95da-73d64f7f6f16/image-24.jpg")
+      let miami = Route(title: "Going to Miami", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/08aa2c57-9703-4db7-8af9-9aef9e945b8d/image-18.jpg")
+      let portland = Route(title: "Going to Portland", photoURL: "https://s3.amazonaws.com/skedaddle-production/uploads/5138b799-9363-4aa7-b909-49e4d5bac5ad/image-26.jpg")
       
-      return [RouteViewModel(route: boston), RouteViewModel(route: toronto), RouteViewModel(route: cape_cod)]
+      return [RouteViewModel(route: boston), RouteViewModel(route: toronto), RouteViewModel(route: cape_cod), RouteViewModel(route: tetons), RouteViewModel(route: boulder), RouteViewModel(route: cape_cod), RouteViewModel(route: los_angeles), RouteViewModel(route: chicago), RouteViewModel(route: portland)]
     }()
     
     return routes
