189012a62f94395ed0d5538780de0f7daa4bb0c4
Lauren N. Roth <laurenroth@gmail.com>
2016-09-20 10:51:03 -0400

    Update Route model to do its own JSON parsing

--- VersionTwo/Route.swift
+++ VersionTwo/Route.swift
@@ -7,13 +7,15 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class Route {
   var title: String
   var photoURL: String
   
-  init(title: String, photoURL: String) {
-    self.title = title
-    self.photoURL = photoURL
+  init(json: AnyObject) {
+    let data = JSON(json)
+    self.title = data["name"].stringValue
+    self.photoURL = data["route_image"].stringValue
   }
 }
